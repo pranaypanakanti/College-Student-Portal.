ca466777ceba9598981ee3fc44f98c25ba76eba1
panakantipranay <panakantipranay@gmail.com>
2026-04-03 10:51:32 +0530

    Initial commit

---
+++ src/main/java/com/studentPortal/InternshipApplication.java
@@ -0,0 +1,13 @@
+package com.studentPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipApplication.class, args);
+	}
+
+}
